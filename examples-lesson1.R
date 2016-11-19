abd6552424c2cbcae1fbc4c1c4afe27721867287
michelescandola <michele.scandola@gmail.com>
2016-11-19 11:17:51 +0100

    Update examples-lesson1.R

--- examples-lesson1.R
+++ examples-lesson1.R
@@ -1,20 +1,7 @@
-# Il prezzo e la spesa in attività promozionali sono due dei fattori che determinano in maniera
-# preponderante le vendite di un prodotto. Supponete che una grande catena di negozi alimentari
-# operante su scala nazionale intenda introdurre una barretta energetica di basso prezzo, chiamata
-# Omnipower. Le barrette energetiche contengono grassi, carboidrati e calorie e forniscono rapidamente
-# energie ai corridori, agli scalatori e agli atleti in genere impegnati in lunghe ed estenuanti attività
-# sportive. Le vendite delle barrette energetiche sono esplose negli ultimi anni e il grande magazzino
-# ritiene che vi possa essere un buon mercato per la Omnipower. Prima di introdurre la barretta in tutti
-# i magazzini, la divisione di marketing della catena intende stabilire l'effetto che il prezzo e le
-# promozioni all'interno dei negozi possono avere sulle vendite.
-# 
-# Un campione di 34 negozi della catena viene selezionato per una ricerca di mercato sulla Omnipower.
-# I negozi hanno tutti approssimativamente il medesimo volume di vendite mensili. Si prendono in
-# considerazioni due variabili indipendenti - il prezzo in centesimi di una barretta Omnipower (X1)
-# e la spesa mensile per le attività promozionali, espressa in dollari, (X2). La spesa promozionale
-# comprende la spesa per i cartelli pubblicitari, i tagliandi di sconto e i campioni gratuiti.
-# La variabile dipendente Y  è il numero di barrette di Omnipower vendute in un mese. Nella Tabella di
-# seguito si riportano i valori osservati per le tre variabili considerate.
+# We have data from an energy bar manafacturer.
+# These data consists in the number of sells, price, and money spent in promotional activities per shop.
+# We want to build the best linear model which fit the number of sells data, considering them as a dependent variable,
+# while the price and the money spent are independent variables.
 # 
 # Negozio 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23  24 25 26 27 28 29 30 31 32 33 34
 # 
@@ -25,3 +12,12 @@
 # 
 # Promozione 200 200 200 200 400 400 400 400 600 600 600 600 200 200 200 200 400 400 400 400 600
 #             600 600 600 200 200 200 200 400 400 400 400 600 600
+
+dati <- data.frame(
+  idStore = 1:34,
+  sells   = c(4141,3842,3056,3519,4226,4630,3507,3754,5000,5120,4011,5015,1916,675,3636,3224,2295,2730,2618,
+          4421,4113,3746,3532,3825,1096,761,2088,820,2114,1882,2159,1602,3354,2927),
+  price   = c(59,59,59,59,59,59,59,59,59,59,59,59,79,79,79,79,79,79,79,79,79,79,79,79,99,99,99,99,99,99,99,99,99,99),
+  prom    = c(200,200,200,200,400,400,400,400,600,600,600,600,200,200,200,200,400,400,400,400,600,
+             600,600,600,200,200,200,200,400,400,400,400,600,600)
+)
